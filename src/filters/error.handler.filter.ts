886a0f47452c2524c479a70bd91e8579ad229526
pallavi bhattarai <67225012+pallavi000@users.noreply.github.com>
2024-02-17 14:03:16 +0200

    add error-filter

---
+++ src/filters/error.handler.filter.ts
@@ -0,0 +1,23 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+import { Request } from 'express';
+
+@Catch()
+export class ErrorHandlerFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const req = host.switchToHttp().getRequest();
+    const res = host.switchToHttp().getResponse();
+    const status = exception.getStatus();
+    const { message }: any = exception.getResponse();
+
+    return res.status(status).json({
+      statusCode: status,
+      path: req.url,
+      message: message,
+    });
+  }
+}
